dbaea5e3c897a6c63bdfa4170afebc7cb445c5f0
Dionatan <dionatan.medeiros@triersistemas.com.br>
2020-02-27 12:48:33 -0300

    feat: Initialized List component

--- src/pages/Users/List/index.js
+++ src/pages/Users/List/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-// import { Container } from './styles';
+import "./styles.css";
 
 export default function List() {
-  return <div />;
+  return (
+    <div className="list">
+      <div className="grid">
+        <div className="header">
+          <div>Nome</div>
+          <div>Perfil</div>
+          <div>Ação</div>
+        </div>
+        <div className="item">
+          <div>Dionatan</div>
+          <div>Admin</div>
+          <div>Editar</div>
+        </div>
+      </div>
+    </div>
+  );
 }
