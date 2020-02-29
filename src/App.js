a23ea8505fc667d4d836e2e3dd090733b3c58dfe
DiouMedeiros <diou.182@gmail.com>
2020-02-28 23:41:13 -0300

    feat: Created a context api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-import "./global.css";
+import TodoList from "./Components/TodoList";
+import { TodoProvider } from "./Components/TodoContext";
+
+import "./styles/global.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoProvider>
+      <div className="App">
+        <TodoList />
+      </div>
+    </TodoProvider>
   );
 }
 
---
+++ src/Components/TodoContext/index.js
@@ -0,0 +1,17 @@
+import React, { useState, createContext } from "react";
+
+export const TodoContext = createContext();
+
+export const TodoProvider = props => {
+  const [todoList, setTodoList] = useState([
+    {
+      task: "Make a good test"
+    }
+  ]);
+
+  return (
+    <TodoContext.Provider value={[todoList, setTodoList]}>
+      {props.children}
+    </TodoContext.Provider>
+  );
+};
