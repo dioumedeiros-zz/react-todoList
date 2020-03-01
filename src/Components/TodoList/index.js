2ae6cfc5c9504247d0e4832ac4f737912354af44
DiouMedeiros <diou.182@gmail.com>
2020-03-01 01:50:01 -0300

    feat: Created todoList control

--- src/Components/TodoList/index.js
+++ src/Components/TodoList/index.js
@@ -1,32 +1,45 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { toast } from "react-toastify";
 
 import { TodoContext } from "../TodoContext";
-import TodoForm from "../TodoForm";
+import Todo from "../TodoList/Todo";
 
 import "./styles.css";
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todoList, setTodoList] = useContext(TodoContext);
+  const [todoFilter, setTodoFilter] = useState();
+
+  useEffect(() => {
+    setTodoFilter(todoList);
+    if (filter && filter !== "T") {
+      const newTodoFilter = todoList.filter(todo => todo.done === filter);
+      setTodoFilter(newTodoFilter);
+    }
+  }, [filter, todoList]);
+
+  function exclude(data) {
+    const newList = todoList.filter(todo => todo.id !== data.id);
+    setTodoList(newList);
+    localStorage.setItem("todoList", JSON.stringify(newList));
+
+    toast.success("Tarefa deletada com sucesso");
+  }
 
   return (
-    <>
-      <TodoForm />
-      <div className="grid">
-        <div className="header">
-          <div>Nome</div>
-          <div>Estado</div>
-          <div>Ações</div>
-        </div>
-        <div className="items">
-          {todoList.map(todo => (
-            <>
-              <div>{todo.task}</div>
-              <div>{todo.done === "S" ? "Finalizada" : ""}</div>
-              <div>Deletar</div>
-            </>
-          ))}
-        </div>
+    <div className="grid">
+      <div className="header">
+        <div>Nome</div>
+        <div>Estado</div>
+        <div>Ações</div>
       </div>
-    </>
+      <div className="items">
+        {todoFilter
+          ? todoFilter.map(todo => (
+              <Todo key={todo.id} data={todo} exclude={exclude} />
+            ))
+          : ""}
+      </div>
+    </div>
   );
 }
