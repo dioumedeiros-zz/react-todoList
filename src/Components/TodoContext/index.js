c2674b31c1c29c2e9ecf5fb851bfd303a94699c3
DiouMedeiros <diou.182@gmail.com>
2020-03-01 01:47:20 -0300

    chore: Updated context using localStorage

--- src/Components/TodoContext/index.js
+++ src/Components/TodoContext/index.js
@@ -1,14 +1,28 @@
-import React, { useState, createContext } from "react";
+import React, { useState, useEffect, createContext } from "react";
 
 export const TodoContext = createContext();
 
 export const TodoProvider = props => {
   const [todoList, setTodoList] = useState([
     {
+      id: 1,
       task: "Make a good test",
       done: "N"
+    },
+    {
+      id: 2,
+      task: "Make it happen",
+      done: "S"
     }
   ]);
+
+  useEffect(() => {
+    const newTodoList = localStorage.getItem("todoList");
+
+    if (newTodoList) {
+      setTodoList(JSON.parse(newTodoList));
+    }
+  }, []);
 
   return (
     <TodoContext.Provider value={[todoList, setTodoList]}>
