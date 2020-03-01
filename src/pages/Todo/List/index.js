20fa1d60137d177260bd2acdd8a0d8d3f44dc8ec
DiouMedeiros <diou.182@gmail.com>
2020-03-01 01:52:25 -0300

    feat: Created listing screen

---
+++ src/pages/Todo/List/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { MdAdd } from "react-icons/md";
+
+import Header from "../../../Components/Header";
+import TodoList from "../../../Components/TodoList";
+import "./styles.css";
+
+export default function List() {
+  const [selection, setSelection] = useState("T");
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <div className="content">
+          <div className="controls">
+            <select
+              className="select-task"
+              onChange={e => setSelection(e.target.value)}
+            >
+              <option value="T">Todas tarefas</option>
+              <option value="S">Tarefas concluídas</option>
+              <option value="N">Tarefas não concluídas</option>
+            </select>
+            <Link to="/register">
+              <button>
+                <MdAdd size={25} />
+                CADASTRAR
+              </button>
+            </Link>
+          </div>
+          <TodoList filter={selection} />
+        </div>
+      </div>
+    </>
+  );
+}
