eaac0d4db1512c9a20bfb5f7d7860477d726ac43
DiouMedeiros <diou.182@gmail.com>
2020-03-01 01:49:17 -0300

    chore: Created form control

--- src/Components/TodoForm/index.js
+++ src/Components/TodoForm/index.js
@@ -1,35 +1,88 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { toast } from "react-toastify";
+import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
+
+import history from "../../services/history";
+
 import { TodoContext } from "../TodoContext";
 
 import "./styles.css";
 
-export default function TodoForm() {
+const schema = Yup.object().shape({
+  task: Yup.string()
+    .max(20, "Máximo de 20 caracteres")
+    .required("A descrição é obrigatória")
+});
+
+export default function TodoForm({ data }) {
   const [task, setTask] = useState("");
+  const [done, setDone] = useState("");
+  const [detail, setDetail] = useState("");
   const [todoList, setTodoList] = useContext(TodoContext);
 
   const state = { YES: "S", NO: "N" };
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTodoList([...todoList, { task, done: state.NO }]);
+  useEffect(() => {
+    if (data) {
+      const todoDetail = todoList.find(todo => todo.id === Number(data));
+      if (todoDetail) {
+        setDetail(todoDetail);
+        setTask(todoDetail.task);
+      }
+    }
+  }, [data, todoList]);
+
+  function handleSubmit() {
+    let list;
+    if (detail) {
+      detail.task = task;
+      detail.done = done ? state.YES : state.NO;
+
+      list = todoList.filter(todo => todo.id !== detail.id);
+      setTodoList(list);
+    }
+
+    const newList = detail ? list : todoList;
+
+    const newDetail = detail
+      ? detail
+      : { id: Math.random(), task, done: done ? state.YES : state.NO };
+
+    const newTodoList = [...newList, newDetail];
+
+    setTodoList(newTodoList);
+    localStorage.setItem("todoList", JSON.stringify(newTodoList));
+    toast.success("Tarefa salva com sucesso");
+    history.push("/");
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <select className="select-task">
-        <option value="T">Todas tarefas</option>
-        <option value="S">Tarefas concluídas</option>
-        <option value="N">Tarefas não concluídas</option>
-      </select>
-      <input
-        className="input-task"
-        type="text"
-        name="task"
-        placeholder="Insira sua atividade"
-        value={task}
-        onChange={e => setTask(e.target.value)}
-      />
-      <button>Submit</button>
-    </form>
+    <div className="container-form">
+      <header>
+        <p>{data ? "Faça sua alteração" : "Faça seu cadastro"}</p>
+      </header>
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input
+          className="input-task"
+          type="text"
+          name="task"
+          placeholder="Insira sua tarefa"
+          value={task}
+          onChange={e => setTask(e.target.value)}
+        />
+
+        <Input
+          type="checkbox"
+          id="done"
+          name="done"
+          value={done}
+          onChange={e => setDone(e.target.checked)}
+        />
+        <label htmlFor="done">Tarefa concluída</label>
+
+        <button>Salvar</button>
+      </Form>
+    </div>
   );
 }
